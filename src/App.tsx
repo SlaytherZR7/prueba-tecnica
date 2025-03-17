a5e444455d1127134f916a1afa8be01b0c0c1f75
SlaytherZR7 <slayther.zamora@epn.edu.ec>
2025-03-17 11:46:34 -0500

    add color toggle, sorting, and country filter functionality in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,41 @@
+import { useState } from 'react';
 import './App.css';
+import { SortBy } from './types.d';
 
 const App = () => {
+  const [showColors, setShowColors] = useState(false);
+  const [sorting, setSorting] = useState<SortBy>(SortBy.NAME);
+  const [filterCountry, setFilterCountry] = useState<string | null>(null);
+
+  const toggleColors = () => {
+    setShowColors(!showColors);
+  };
+
+  const toggleSortByCountry = () => {
+    const newSortingValue =
+      sorting === SortBy.NONE ? SortBy.COUNTRY : SortBy.NONE;
+    setSorting(newSortingValue);
+  };
+
   return (
     <div className="content">
-      <h1>Rsbuild with React</h1>
-      <p>Start building amazing things with Rsbuild.</p>
+      <h1>Prueba técnica</h1>
+      <header>
+        <button onClick={toggleColors} type="button">
+          Colorear filas
+        </button>
+        <button onClick={toggleSortByCountry} type="button">
+          {sorting === SortBy.COUNTRY
+            ? 'No ordenar por país'
+            : 'Ordenar por país'}
+        </button>
+        <input
+          placeholder="Filtra por país"
+          onChange={(e) => {
+            setFilterCountry(e.target.value);
+          }}
+        />
+      </header>
     </div>
   );
 };
