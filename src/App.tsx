efd0a0a0ba4b2d0f6391ed23be35cba6a6eefdb6
SlaytherZR7 <slayther.zamora@epn.edu.ec>
2025-03-17 11:58:35 -0500

    add user management features: fetch users, sorting, filtering, and deletion; integrate UsersList component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
-import { useState } from 'react';
+import { useEffect, useMemo, useRef, useState } from 'react';
 import './App.css';
-import { SortBy } from './types.d';
+import { UsersList } from './components/UsersList';
+import { SortBy, type User } from './types.d';
 
 const App = () => {
+  const [users, setUsers] = useState<User[]>([]);
   const [showColors, setShowColors] = useState(false);
   const [sorting, setSorting] = useState<SortBy>(SortBy.NAME);
   const [filterCountry, setFilterCountry] = useState<string | null>(null);
+
+  const originalUsers = useRef<User[]>([]);
 
   const toggleColors = () => {
     setShowColors(!showColors);
@@ -16,6 +20,58 @@ const App = () => {
       sorting === SortBy.NONE ? SortBy.COUNTRY : SortBy.NONE;
     setSorting(newSortingValue);
   };
+
+  const handleReset = () => {
+    setUsers(originalUsers.current);
+  };
+
+  const handleDelete = (email: string) => {
+    const filteredUsers = users.filter((user) => user.email !== email);
+    setUsers(filteredUsers);
+  };
+
+  const handleSort = (sort: SortBy) => {
+    setSorting(sort);
+  };
+
+  useEffect(() => {
+    fetch('https://randomuser.me/api?results=100')
+      .then(async (res) => await res.json())
+      .then((res) => {
+        setUsers(res.results);
+        originalUsers.current = res.results;
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }, []);
+
+  const filteredUsers = useMemo(() => {
+    return filterCountry != null && filterCountry.length > 0
+      ? users.filter((user) => {
+          return user.location.country
+            .toLowerCase()
+            .includes(filterCountry.toLowerCase());
+        })
+      : users;
+  }, [users, filterCountry]);
+
+  const sortedUsers = useMemo(() => {
+    console.log('calculate sortedUsers');
+
+    if (sorting === SortBy.NONE) return filteredUsers;
+
+    const compareProperties: Record<string, (user: User) => any> = {
+      [SortBy.COUNTRY]: (user) => user.location.country,
+      [SortBy.NAME]: (user) => user.name.first,
+      [SortBy.LAST]: (user) => user.name.last,
+    };
+
+    return filteredUsers.toSorted((a, b) => {
+      const extractProperty = compareProperties[sorting];
+      return extractProperty(a).localeCompare(extractProperty(b));
+    });
+  }, [filteredUsers, sorting]);
 
   return (
     <div className="content">
@@ -29,6 +85,9 @@ const App = () => {
             ? 'No ordenar por país'
             : 'Ordenar por país'}
         </button>
+        <button onClick={handleReset} type="button">
+          Resetear estado
+        </button>
         <input
           placeholder="Filtra por país"
           onChange={(e) => {
@@ -36,6 +95,14 @@ const App = () => {
           }}
         />
       </header>
+      <main>
+        <UsersList
+          changeSorting={handleSort}
+          deleteUser={handleDelete}
+          showColors={showColors}
+          users={sortedUsers}
+        />
+      </main>
     </div>
   );
 };
